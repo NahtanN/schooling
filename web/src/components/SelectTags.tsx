3d3e5aaf1c737e60c5b474d2697a05c946fab206
Nathan Gomes <nahtann@outlook.com>
2021-01-15 17:49:37 -0300

    selected tag component finished

--- web/src/components/SelectTags.tsx
+++ web/src/components/SelectTags.tsx
@@ -1,14 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import API from '../service/api';
 
 import '../styles/components/SelectTags.css';
 
-export default function SelectTags() {
+interface Props {
+    tagsId: (tag: string[]) => void;
+}
+
+interface TagType {
+    id: number;
+    tag: string;
+}
+
+const SelectTags: React.FC<Props> = ({ tagsId }) => {
+    const [tags, setTags] = useState<TagType[]>([]);
+    const [ selectedTags, setSelectedTags ] = useState<string[]>([]);
+    
+    useEffect(() => {
+        API.get('/fetch-tags')
+            .then(tags => {
+                setTags(tags.data);
+            });
+    }, []);
+
+    const handleSelectedTags = (tag: string) => {
+        const findTag = selectedTags.includes(tag);        
+        
+        findTag ? (setSelectedTags(selectedTags.filter(item => item !== tag)))
+                : (setSelectedTags([...selectedTags, tag]));
+
+    }    
+
+    const handleDoubleClick = (tag: string) => {
+        var temp = selectedTags.filter(item => item !== tag);
+        temp.unshift(tag);
+
+        setSelectedTags(temp);
+    }
+
+    tagsId(selectedTags);
+    
     return (
         <ul className="tag-options">
-            <li>Economia</li>
-            <li>História</li>
-            <li>Lieteratura</li>
-            <li>Política</li>                        
-        </ul>        
+            { tags.map(tag => {                
+                let tagClass;
+
+                if (selectedTags[0] === tag.tag) {
+                    tagClass = 'selected-thumb-tag';
+                } 
+
+                if (selectedTags.includes(tag.tag) && tag.tag !== selectedTags[0]) {
+                    tagClass = 'selected-tag'
+                }
+
+                return <li 
+                            key={tag.id}
+                            onClick={() => handleSelectedTags(tag.tag)}
+                            onDoubleClick={() => handleDoubleClick(tag.tag)}
+                            className={tagClass}
+                        >
+                            {tag.tag}
+                        </li>}
+            )}
+        </ul>
     );
 }
+
+export default SelectTags
